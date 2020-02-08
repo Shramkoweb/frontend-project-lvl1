38b99f40da24bf73368cf1e0a3faf63e5012620f
serhii.shramko <shramko.web@yahoo.com>
2020-02-08 22:58:37 +0200

    style: linter fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    console.log('Welcome to the Brain Games!\n');
+  console.log('Welcome to the Brain Games!\n');
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
