c9963b155063fe76cf5f6e3373531ad347a3a2eb
serhii.shramko <shramko.web@yahoo.com>
2020-02-23 22:23:13 +0200

    refactor: calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,17 +1,21 @@
 import hexletPairs from '@hexlet/pairs';
 
 import { playGame } from '../index.js';
-import { getRandomArbitrary, getRandomNumber } from '../utils.js';
-import { RULE } from '../constants.js';
+import { getRandomArbitrary, getRandomNumbers } from '../utils.js';
 
 const OPERATORS_AMOUNT = 3;
+const RULE = 'What is the result of the expression?';
+
+const NUMBER_RANGE = {
+  MIN: 0,
+  MAX: 15,
+};
 
 const generateConditions = () => {
   let question; // eslint fix
   let answer; // eslint fix
+  const [firstNumber, secondNumber] = getRandomNumbers(2, NUMBER_RANGE.MIN, NUMBER_RANGE.MAX);
   const operator = getRandomArbitrary(0, OPERATORS_AMOUNT);
-  const firstNumber = getRandomNumber();
-  const secondNumber = getRandomNumber();
 
   switch (operator) {
     case 0:
@@ -30,6 +34,6 @@ const generateConditions = () => {
   return hexletPairs.cons(question, answer.toString(10));
 };
 
-const playCalc = () => playGame(RULE.CALC, generateConditions);
+const playCalc = () => playGame(RULE, generateConditions);
 
 export default playCalc;
