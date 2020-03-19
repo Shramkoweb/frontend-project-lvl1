309a9b96c34f5c326eeca4ef403d8dc94def1dd2
serhii.shramko <shramko.web@yahoo.com>
2020-03-19 20:34:37 +0200

    refactor: index make more clear

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { car, cdr } from './pairs.js';
 
 const NUMBER_OF_ROUNDS = 3;
 
-// return 'false' if game over or 'true' if win
-const playRounds = (getGameConditions, roundsToWin) => {
+const isGameWin = (getGameConditions, roundsToWin, userName) => {
   for (let i = 1; i <= roundsToWin; i += 1) {
     const gameConditions = getGameConditions(); // get game conditions pair
     const question = car(gameConditions);
@@ -18,10 +17,12 @@ const playRounds = (getGameConditions, roundsToWin) => {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;( Correct answer was '${answer}'`);
+      console.log(`Let's try again, ${userName}!`);
       return false;
     }
   }
 
+  console.log(`Congratulations, ${userName}!`);
   return true;
 };
 
@@ -31,13 +32,7 @@ const playGame = (rulesOfTheGame, getGameConditions) => {
   console.log(`Hello, ${userName}\n`);
   console.log(`${rulesOfTheGame}\n`);
 
-  const canPlayNextRound = playRounds(getGameConditions, NUMBER_OF_ROUNDS);
-
-  if (canPlayNextRound) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log(`Let's try again, ${userName}!`);
-  }
+  isGameWin(getGameConditions, NUMBER_OF_ROUNDS, userName);
 };
 
 export default playGame;
